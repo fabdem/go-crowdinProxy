56f7748c830df1248c6503c53f092c88a9e391bf
fabdem <demurger@gmail.com>
2019-02-27 17:57:30 -0800

    Update comments

--- crowdinProxy.go
+++ crowdinProxy.go
@@ -13,7 +13,7 @@ import (
 var connectionTOinSecs time.Duration = 5
 var readwriteTOinSecs time.Duration = 40
 
-// Set connection and read/write timeouts for the subsequent new connections 
+// Set connection and read/write timeouts for the subsequent new connections
 func SetTimeouts(cnctTOinSecs, rwTOinSecs int) {
     connectionTOinSecs = time.Duration(cnctTOinSecs)
     readwriteTOinSecs = time.Duration(rwTOinSecs)
@@ -23,25 +23,32 @@ func SetTimeouts(cnctTOinSecs, rwTOinSecs int) {
 // New - create a new instance of crowdin  use of a PROXY.
 func New(token, project, proxy string) (*crowdin.Crowdin, error) {
 
-	proxyUrl, err := url.Parse(proxy)
-	if err != nil {
-		fmt.Println("Bad proxy URL", err)
-		return nil,err
-	} 
+	// transport httpclient.Transport
 
-    api := crowdin.New(token, project)
-	
+	api := crowdin.New(token, project)
+
+	var proxyUrl *url.URL
+	var err error
+
+	if len(proxy) > 0 {     // If a proxy is defined
+		proxyUrl, err = url.Parse(proxy)
+		if err != nil {
+			fmt.Println("Bad proxy URL", err)
+			return nil,err
+		}
+	}
+
 	// Set proxy and timeouts
 	transport := &httpclient.Transport{
 		ConnectTimeout:   connectionTOinSecs * time.Second,
 		ReadWriteTimeout: readwriteTOinSecs * time.Second,
 		Proxy: http.ProxyURL(proxyUrl),
 	}
-	
+
+
+
 	api.SetClient(&http.Client{
 		Transport: transport,
 	})
-	
 	return api, nil
 }
-
